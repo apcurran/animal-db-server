6380d06ec02a47403758d107f6e6c5198a0ecb78
apc <alexcurran.va@gmail.com>
2020-06-04 13:10:46 -0500

    Added route endpoints for post, patch, and delete animal

--- api/routes/animals.js
+++ api/routes/animals.js
@@ -1,1 +1,100 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+// Animal model
+const Animal = require("../models/Animal");
+
+router.get("/", async (req, res) => {
+    try {
+        const animals = await Animal.find();
+
+        res.status(200).json(animals);
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+});
+
+router.post("/animal", async (req, res) => {
+    // Destructure values from req.body obj
+    const {
+        animal_common_name,
+        animal_scientific_name,
+        animal_type,
+        animal_diet,
+        animal_lifespan,
+        animal_size,
+        animal_weight,
+        animal_fact,
+        main_img_url
+    } = req.body;
+
+    const animal = new Animal({
+        animal_common_name,
+        animal_scientific_name,
+        animal_type,
+        animal_diet,
+        animal_lifespan,
+        animal_size,
+        animal_weight,
+        animal_fact,
+        main_img_url 
+    });
+
+    try {
+        await animal.save();
+        
+        res.status(201).json({
+            message: "New animal added!"
+        });
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+});
+
+// PATCH an existing animal's data
+router.patch("/animal/:search_term", async (req, res) => {
+    const { search_term } = req.params;
+    const regexSearch = new RegExp(search_term, "i");
+    const query = { animal_common_name: regexSearch }; // Search for animal case-insensitive
+
+    try {
+        await Animal.findOneAndUpdate(query, { $set: req.body });
+    
+        res.status(201).json({
+            message: "Animal data updated!"
+        });
+        
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            error: err
+        });
+    }
+});
+
+router.delete("/animal/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Animal.findByIdAndDelete(id);
+
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            error: err
+        });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const morgan = require("morgan");
 // Import Routes
-
+const animalsRouter = require("./api/routes/animals");
 
 const app = express();
 
 // DB Connection
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useFindAndModify', false);
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Mongo connection error"));
 
@@ -24,8 +26,7 @@ if (process.env.NODE_ENV === "development") {
 app.use(cors()); // Enable cors for all routes
 app.use(express.json());
 
-
-
 // Routes
+app.use("/api/animals", animalsRouter);
 
 app.listen(PORT, () => console.log(`Server running on port, ${PORT}.`));
