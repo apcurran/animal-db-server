f1eaa6d78e36c93eccfe3e3519d367c51f1a8495
apc <alexcurran.va@gmail.com>
2020-06-10 11:54:03 -0500

    Removed client folder

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const morgan = require("morgan");
@@ -25,20 +24,11 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
-
 app.use(cors()); // Enable cors for all routes
 app.use(express.json());
 
 // Routes
 app.use("/api/animals", animalsRouter);
 app.use("/api/user", userRouter);
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server running on port, ${PORT}.`));
